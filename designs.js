dd6c30a00143249fd154c8f28cd3fa480566eb23
Tomas Guerreiro <falatomas@gmail.com>
2018-03-23 16:49:05 -0300

    funçoes iniciadas

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
 // Select size input
 
+const pixelCanvas =  $('#pixelCanvas');
+
+
 // When size is submitted by the user, call makeGrid()
+function makeGrid(height, weight) {
+    console.log(height);
+    console.log(weight);
 
-function makeGrid() {
-
-// Your code goes here!
+    //Cria o GRID
+    for (let h = 0; h < height; h++) {
+        pixelCanvas.append('<tr></tr>');      
+        for (let w = 0; w < weight; w++) {
+            $('tr:last-child').append('<td></td>');           
+        }       
+    }
 
 }
+
+$(pixelCanvas).on('click', 'td', function(){
+
+});
+
+
+$('#sizePicker').submit(function(e){
+    e.preventDefault();
+    let inputHeight = $('#inputHeight').val();
+    let inputWeight = $('#inputWeight').val();
+    makeGrid(inputHeight, inputWeight);
+});
