812e254bf841b144f808b303c8f35328cb2d9e6a
Tomas Guerreiro <falatomas@gmail.com>
2018-03-23 17:44:49 -0300

    finale

--- designs.js
+++ designs.js
@@ -1,26 +1,27 @@
 // Select color input
 // Select size input
 
-const pixelCanvas =  $('#pixelCanvas');
+const pixelCanvas = $('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid(height, weight) {
+    //Limpa o GRID
+    $(pixelCanvas).empty();
     //Cria o GRID
     for (let h = 0; h < height; h++) {
-        pixelCanvas.append('<tr></tr>');      
+        pixelCanvas.append('<tr></tr>');
         for (let w = 0; w < weight; w++) {
-            $('tr:last-child').append('<td></td>');           
-        }       
+            $('tr:last-child').append('<td></td>');
+        }
     }
-
 }
 
-$(pixelCanvas).on('click', 'td', function(){
+//Pinta a celula selecionada
+$(pixelCanvas).on('click', 'td', function () {
     $(this).css("background-color", $("#colorPicker").val());
 });
 
-
-$('#sizePicker').submit(function(e){
+$('#sizePicker').submit(function (e) {
     e.preventDefault();
     let inputHeight = $('#inputHeight').val();
     let inputWeight = $('#inputWeight').val();
